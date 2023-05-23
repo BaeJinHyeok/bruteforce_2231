21e617f0cc995f23e5683f852197c06453537459
Jh <gumemoge@gmail.com>
2023-05-23 23:22:11 +0900

    blackjack_2798

---
+++ blackjack_2798/src/Main.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int N;
+    private static int M;
+
+    private static BufferedReader br;
+    private static StringTokenizer st;
+
+    private static int[] arr;
+    private static int temp = 0;
+    private static int result = 0;
+    public static void main(String[] args) throws IOException {
+
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        for ( int i =0; i<N; i++){
+            for(int j=i+1; j<N; j++){
+                for(int k=j+1; k<N; k++){
+                    temp = arr[i]+arr[j]+arr[k];
+
+                    if(temp == M){
+                        System.out.println(temp);
+                    }
+                    if(temp < M && result < temp){
+                        result = temp;
+                    }
+
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
