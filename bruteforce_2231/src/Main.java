2ebe75333be3531007636c4baf53a4d056ac496e
Jh <gumemoge@gmail.com>
2023-05-30 00:29:05 +0900

    bj_2231

---
+++ bruteforce_2231/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    private static int N;
+    private static int result;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        for(int i = 0; i<N; i++) {
+            int num = i;
+            int sum = 0;
+
+            while (num != 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            if( sum + i == N){
+                result = i;
+                break;
+            }
+
+        }
+
+
+        System.out.println(result);
+
+    }
+}
